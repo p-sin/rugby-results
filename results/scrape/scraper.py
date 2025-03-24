81550ad051b3ad80026b7bca88f314a3de3534a0
p-sin <p-sinfield@live.co.uk>
2025-03-24 11:14:59 +0000

    Split functions into util file

--- results/scrape/scraper.py
+++ results/scrape/scraper.py
@@ -1,68 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
-from requests.adapters import HTTPAdapter, Retry
+from dataclasses import dataclass
+from typing import Any
+
+from results.scrape import scrape_utils as utils
 
 
-def retrieve_url(url: str) -> str:
-    """Retrieve the content of a URL as string.
+@dataclass
+class Scraper:
+    config: dict[Any, Any]
 
-    Args:
-        url (str): The URL to retrieve.
-
-    Returns:
-        str: The content of the URL
-    """
-    session = requests.Session()
-    retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
-    session.mount("http://", HTTPAdapter(max_retries=retries))
-
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    response = session.get(url, headers=headers, timeout=60)
-    return response.text
+    def scrape(self) -> None:
+        utils.make_soup(self.config["url"])
 
 
-def make_soup(url: str) -> BeautifulSoup:
-    """Create a BeautifulSoup object from a URL.
-
-    Args:
-        url (str): The URL to scrape.
-
-    Returns:
-        BeautifulSoup: The BeautifulSoup object.
-    """
-    html = retrieve_url(url)
-    return BeautifulSoup(html, "html.parser")
-
-
-def extract_event(event_class: str) -> str:
-    """Extract the type of event from the event string.
-
-    Args:
-        event_class (str): The class name containing the event type
-
-    Returns:
-        str: The type of event.
-    """
-    events: dict[str, str] = {
-        "try": "Try",
-        "conversion": "Conversion",
-        "penalty": "Penalty",
-        "drop-goal": "Drop Goal",
-        "yellow-card": "Yellow Card",
-        "red-card": "Red Card",
-        "penalty-try": "Penalty Try",
-    }
-
-    for key, value in events.items():
-        if key in event_class:
-            return value
-
-    err = f"Event type not found: {event_class}"
-    raise ValueError(err)
-
-
-soup = make_soup(
+soup = utils.make_soup(
     "https://www.englandrugby.com/fixtures-and-results/search-results?competition=173&division=57597&season=2024-2025#results"
 )
 
@@ -82,14 +32,16 @@ result_wrappers = card_container.find_all(
 for wrapper in result_wrappers:
     # The first div contains the date component
     date_div = wrapper.find("div").find("div")
-    print(date_div.text)
+    print(f"Date of match: {date_div.text}")
 
     results = wrapper.find_all("div", recursive=False)[1:]
 
     for result in results:
         fixture_data = result.find("div")
-        print(fixture_data.find("div").find("a").text)
-        print(fixture_data.find_all("div", recursive=False)[2].find("a").text)
+        print(f"Home team: {fixture_data.find("div").find("a").text}")
+        print(
+            f"Away team: {fixture_data.find_all("div", recursive=False)[2].find("a").text}"
+        )
 
         scores = [
             score.text
@@ -97,13 +49,15 @@ for wrapper in result_wrappers:
                 "span"
             )
         ]
-        print(scores)
+        print(f"Scores: {scores}")
 
         match_info_url = (
             result.find("div", class_="coh-style-clicktoinfo").find("a").get("href")
         )
 
-        match_info = make_soup(url=f"https://www.englandrugby.com/{match_info_url}")
+        match_info = utils.make_soup(
+            url=f"https://www.englandrugby.com/{match_info_url}"
+        )
         all_events = match_info.find_all(
             "div", class_="c042-key-match-events-accordion-body-info"
         )
@@ -111,20 +65,24 @@ for wrapper in result_wrappers:
         home_events = all_events[0].find_all(
             "div", class_="c042-key-match-events-items"
         )
-        for event in home_events:
+        for event_num, event in enumerate(home_events):
             try:
                 event_class = event.find("div").get("class")
-                print(extract_event(event_class[1]))
+                print(
+                    f"Home event ({event_num}): {utils.extract_event(event_class[1])}"
+                )
             except AttributeError:
                 continue
 
         away_events = all_events[2].find_all(
             "div", class_="c042-key-match-events-items"
         )
-        for event in away_events:
+        for event_num, event in enumerate(away_events):
             try:
                 event_class = event.find("div").get("class")
-                print(extract_event(event_class[1]))
+                print(
+                    f"Away event ({event_num}): {utils.extract_event(event_class[1])}"
+                )
             except AttributeError:
                 continue
 
